674bdc4e2792b4e9f0476144a9679cbb7a197585
elanore <elanorejoseph@gmail.com>
2024-08-18 13:57:00 +0530

    more programs

--- index.js
+++ index.js
@@ -20,12 +20,78 @@
 
 //program 3- viceversa of above scenario
 
-function reverseFormatString(str) {
-  return str
-    .split(" ")
-    .map((word) => word.charAt(0).toLowerCase() + word.slice(1).toLowerCase())
-    .join("_");
-}
+// function reverseFormatString(str) {
+//   return str
+//     .split(" ")
+//     .map((word) => word.charAt(0).toLowerCase() + word.slice(1).toLowerCase())
+//     .join("_");
+// }
 
-const reverseString = reverseFormatString("Ui dev hello");
-console.log(reverseString);
+// const reverseString = reverseFormatString("Ui dev hello");
+// console.log(reverseString);
+
+//pgm4:Find the occurrence in given array in JavaScript
+// function countOccurrence(arr) {
+//   const occurenceCount = {};
+//   arr.forEach((element) => {
+//     occurenceCount[element] = (occurenceCount[element] || 0) + 1;
+//   });
+//   console.log(occurenceCount);
+// }
+
+//   const arr = [1, 5, 4, 3, 2, 2, 6, 1, 7, 2, 3, 8];
+//   countOccurrence(arr);
+
+//pgm5: find occurance in given array of objects
+
+// const people = [
+//   { name: "shiny", age: 32, gender: "female" },
+//   { name: "hello", age: 1, gender: "male" },
+//   { name: "happy", age: 4, gender: "male" },
+// ];
+
+// function countOccurrence(arr, property) {
+//   const occurenceCount = {};
+//   arr.forEach((person) => {
+//     const value = person[property];
+//     occurenceCount[value] = (occurenceCount[value] || 0) + 1;
+//   });
+
+//   return occurenceCount;
+// }
+// const genderCounts = countOccurrence(people, "gender");
+// //only male
+// // const numberOfMales = genderCounts["male"] || 0;
+// // console.log(numberOfMales);
+// console.log(genderCounts, "gender Counts");
+
+// const ageCounts = countOccurrence(people,'age');
+// console.log(ageCounts,"Age counts");
+
+// age above 18 only
+// const people = [
+//   { name: "shiny", age: 32, gender: "female" },
+//   { name: "hello", age: 1, gender: "male" },
+//   { name: "happy", age: 4, gender: "male" },
+// ];
+
+// const numberOfAdults = people.reduce((count, person) => {
+//   return person.age > 18 ? count + 1 : count;
+// }, 0);
+
+// console.log(numberOfAdults);
+
+// OR
+// const people = [
+//     { name: 'shiny', age: 32, gender: 'female' },
+//     { name: 'hello', age: 1, gender: 'male' },
+//     { name: 'happy', age: 4, gender: 'male' }
+// ];
+
+// // First, filter the people who are older than 18
+// const adults = people.filter(person => person.age > 18);
+
+// // Get the count of the filtered adults
+// const numberOfAdults = adults.length;
+
+// console.log(numberOfAdults);  // Output: 1
