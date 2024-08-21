bbbc9e6615e92ce3d29d1f6dfc6f4378f77fc75a
elanore <elanorejoseph@gmail.com>
2024-08-21 18:28:18 +0530

    reversed string

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,13 @@
 let str = "hello world";
 let reversedString = "";
 
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedString = reversedString + str[i];
-}
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversedString = reversedString + str[i];
+// }
 
-console.log(reversedString, "Reversed");
+// console.log(reversedString, "Reversed");
+
+// console.log(
+//   (reverseString = str.split("").reverse(str).join("")),
+//   "builtinreverse"
+// );
