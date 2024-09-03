bb86e295b9c40c73d7e82ff2223c7f270e975de0
elanore <elanorejoseph@gmail.com>
2024-09-03 20:21:01 +0530

    roman to number

---
+++ romantointeger.js
@@ -0,0 +1,28 @@
+function romanToInt(s) {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const currentVal = romanMap[s[i]];
+    const nextVal = romanMap[s[i + 1]];
+
+    if (nextVal > currentVal) {
+      total -= currentVal;
+    } else {
+      total += currentVal;
+    }
+  }
+
+  return total;
+}
+
+console.log(romanToInt("LVIII"),"roman to integer");
