97308c877b20d74ffcd91456c040660ce955acd8
elanore <elanorejoseph@gmail.com>
2024-11-30 20:14:44 +0530

    is anagram

--- validanagram.js
+++ validanagram.js
@@ -1,31 +1,10 @@
-var isAnagram = function (s, t) {
-  // If the lengths of the two strings are not equal, they can't be anagrams
-  if (s.length !== t.length) {
-    return false;
-  }
+function isAnagram(s, t) {
+  // If lengths are not equal, they can't be anagrams
+  if (s.length !== t.length) return false;
 
-  // Create frequency maps for both strings
-  const charCountS = {};
-  const charCountT = {};
-
-  // Populate the frequency maps
-  for (let char of s) {
-    charCountS[char] = (charCountS[char] || 0) + 1;
-  }
-
-  for (let char of t) {
-    charCountT[char] = (charCountT[char] || 0) + 1;
-  }
-
-  // Compare the frequency maps
-  for (let key in charCountS) {
-    if (charCountS[key] !== charCountT[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+  // Sort the characters in both strings and compare
+  return s.split("").sort().join("") === t.split("").sort().join("");
+}
 
 // Example 1
 console.log(isAnagram("anagram", "nagaram")); // Output: true
