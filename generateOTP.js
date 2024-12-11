9b1d11f5e895f09104ba5c4f5217dfc87c5b9f0f
elanore <elanorejoseph@gmail.com>
2024-12-11 20:20:10 +0530

    code change for otp

--- generateOTP.js
+++ generateOTP.js
@@ -2,9 +2,9 @@ function generateRandomotp() {
   const chars = "0123456789";
   let strlength = 6;
   let result = "";
-  for ( let i = 0; i < strlength; i++) {
+  for (let i = 0; i < strlength; i++) {
     let randNum = Math.floor(Math.random() * chars.length);
-    result = result + chars.substring(randNum, randNum + 1);
+    result = result + chars.charAt(randNum);
   }
   document.getElementById("otp").innerHTML = result;
 }
